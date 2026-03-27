9cca524fac4aa2dcacfb187087858d37426f5983
rockbox-y1 <220063322+rockbox-y1@users.noreply.github.com>
2026-03-28 00:36:30 +0100

    Fixes WPS hotkey plugins

--- firmware/export/rbpaths.h
+++ firmware/export/rbpaths.h
@@ -91,8 +91,7 @@
 #if defined(APPLICATION) && \
         !(defined(SAMSUNG_YPR0) || defined(SAMSUNG_YPR1) || \
           defined(DX50) || defined(DX90) || defined(SONY_NWZ_LINUX) || \
-          defined(HIBY_LINUX) || defined(FIIO_M3K_LINUX) || defined(CTRU) || \
-          defined(PLATFORM_INNIOASIS_Y1))
+          defined(HIBY_LINUX) || defined(FIIO_M3K_LINUX) || defined(CTRU))
 
 #define PLUGIN_DATA_DIR          ROCKBOX_DIR "/rocks.data"
 #define PLUGIN_GAMES_DATA_DIR    PLUGIN_DATA_DIR
